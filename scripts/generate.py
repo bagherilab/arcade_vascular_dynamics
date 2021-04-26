c8406db96a9b36568ea035e865077790d52ee9ad
jessicasyu <jessicayu@u.northwestern.edu>
2021-04-26 14:29:06 -0400

    Added type grid figure

--- scripts/generate.py
+++ scripts/generate.py
@@ -55,3 +55,44 @@ def save_point_distances(file, extension, out):
     """Save merged point distances file."""
     save_csv(file, ','.join(out['header']) + "\n", zip(*out['data']), extension)
 
+# SITE ARCHITECTURE: TYPE GRID =================================================
+
+def make_type_grid_borders(D, R, H, T, N, C, POPS, TYPES, outfile, code, exclude=[-1], timepoints=[], seeds=[]):
+    """Extract type grid borders."""
+    d = np.take(D["agents"], timepoints, axis=1)
+    TT = [T[i] for i in timepoints]
+    
+    inds = [[get_inds(d, j, i, H, exclude)
+        for j in range(0, N)]
+        for i in range(0, len(TT))]
+    
+    all_inds = [x for ind in inds[0] for x in ind]
+    lines = lin = get_spatial_outlines(C, 1, R, H, [all_inds])
+    outline = []
+    xy, offx, offy, L, W = convert(C, R)
+
+    for z, li in zip(range(1 - H, H), lin):
+        outline = outline + [[i - offx, j - offy, z, k, C]
+            for i, A in enumerate(li)
+            for j, B in enumerate(A)
+            for k, C in enumerate(B) if C != 0]
+
+    header = "x,y,z,DIRECTION,WEIGHT\n"
+    save_csv(f"{outfile}{code}", header, zip(*outline), f".BORDERS.{format_time(TT[0])}")
+
+def merge_type_grid_borders(file, out, keys, extension, code, tar=None):
+    """Merge type grid border files across conditions."""
+    filepath = f"{file}{code}{extension}.150.csv"
+
+    if tar:
+        D = load_csv(filepath.split("/")[-1], tar=tar)
+    else:
+        D = load_csv(filepath)
+
+    d = [[keys['context'], keys['sites'].replace("SOURCE_", "")] + e for e in D[1:]]
+    out['data'] = out['data'] + d
+    out['header'] = ["context", "grid"] + D[0]
+
+def save_type_grid_borders(file, extension, out):
+    """Save merged type grid border files."""
+    save_csv(file, ','.join(out['header']) + "\n", zip(*out['data']), extension)
